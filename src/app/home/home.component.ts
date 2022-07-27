994e0df94be0a3feef8e3a103b668d7cfe8698d6
zidiks <zidiks228@gmail.com>
2022-07-27 17:27:13 +0300

    fix: build commands

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "../../core/commons/component";
 import { ComponentInstance } from "../../core/models/component-instance.model";
-import html from "home.component.html";
+import html from "./home.component.html";
 import scss from "./home.component.scss";
 
 export class HomeComponent extends Component implements ComponentInstance {
